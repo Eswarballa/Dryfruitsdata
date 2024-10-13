efe74d9ddf25b411d56619af20878c30236ca1dd
Eswar Balla <107669283+Eswarballa@users.noreply.github.com>
2024-10-13 16:26:11 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -13,6 +13,16 @@
       "image": "https://i.gadgets360cdn.com/large/amazon_great_indian_festival_sale_2021_image_1_1633097531416.jpg?downsize=950:*",
       "link": "https://www.gadgets360.com/laptops/features/amazon-great-indian-festival-2021-sale-today-best-offers-on-electronics-cameras-earphones-office-chair-2568569",
       "alt": "Great India Festival"
+    },
+    {
+      "image": "https://media.gettyimages.com/id/84380314/photo/trader-watching-stocks-crash-on-screen.jpg?s=612x612&w=0&k=20&c=kC-7gZBODeAWJvwYRsYV9FPkqTwgOPkLoOOpCNIGZ0E=",
+      "link": "https://www.cnbc.com/world/?region=world",
+      "alt": "Stocks"
+    },
+    {
+      "image": "https://www.91-cdn.com/hub/wp-content/uploads/2023/09/Screenshot-2024-09-16-at-4.21.09%E2%80%AFPM-1024x508.png",
+      "link": "https://www.91mobiles.com/hub/flipkart-upcoming-sale-offers-date/",
+      "alt": "flipcart"
     }
   ]
   
