9fb3c44d3efc08dd6cd8cd17deb4ae4722d34d52
Eswar Balla <107669283+Eswarballa@users.noreply.github.com>
2024-10-13 16:04:18 +0530

    Create adds.js

---
+++ adds.js
@@ -0,0 +1,18 @@
+[
+    {
+      "image": "https://couponwish.in/wp-content/uploads/2021/12/kfc-wednesday-offer-today.jpg",
+      "link": "https://couponwish.in/blog/kfc-wednesday-offer/",
+      "alt": "KFC WEDNESDAY DEAL"
+    },
+    {
+      "image": "https://img.paisawapas.com/ovz3vew9pw/2024/10/01111111/Myntra-BFF-Offers-on-Kurta-Sets.jpg",
+      "link": "https://www.paisawapas.com/blog/upcoming-zara-online-sale-with-dates/",
+      "alt": "Big Billion day"
+    },
+    {
+      "image": "https://i.gadgets360cdn.com/large/amazon_great_indian_festival_sale_2021_image_1_1633097531416.jpg?downsize=950:*",
+      "link": "https://www.gadgets360.com/laptops/features/amazon-great-indian-festival-2021-sale-today-best-offers-on-electronics-cameras-earphones-office-chair-2568569",
+      "alt": "Great India Festival"
+    }
+  ]
+  
